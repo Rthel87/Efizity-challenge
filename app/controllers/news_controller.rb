bc29ee60c603375232e0786dd768fe5d22b7b452
Rodrigo <rod.castillo.c@gmail.com>
2023-12-07 21:28:26 -0300

    News: Se define función 'create' del controlador 'News'

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -3,6 +3,14 @@ class NewsController < ApplicationController
   end
 
   def create
+    news = News.new(news.params)
+    if news.valid?
+      news.save!
+      flash[:info] = "Se ha agregado la noticia"
+      redirect_to news_index
+    else
+      flash[:error] = "Error. No se ha guardado la noticia. Verifique los datos ingresados."
+    end
   end
 
   def new
